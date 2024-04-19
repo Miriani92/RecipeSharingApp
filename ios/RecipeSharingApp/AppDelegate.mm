45f357fb78a6096ab9b9636f148652d77f447e4c
Miriani92 <mac@Miriani.local>
2024-04-19 13:04:27 +0400

    init

--- ios/RecipeSharingApp/AppDelegate.mm
+++ ios/RecipeSharingApp/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -6,12 +7,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"RecipeSharingApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+      
+      self.moduleName = @"RecipeSharingApp";
+      // You can add your custom initial props in the dictionary below.
+      // They will be passed down to the ViewController used by React Native.
+      self.initialProps = @{};
+      
+      return [super application:application didFinishLaunchingWithOptions:launchOptions];
+
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
