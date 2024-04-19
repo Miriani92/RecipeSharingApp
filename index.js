b8531d0e47106b1a6ca21f9072f9832a3ed19f71
Miriani92 <mac@Miriani.local>
2024-04-19 17:10:58 +0400

    add storybook

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /**
  * @format
  */
-
+import Storybook from './.storybook';
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {App} from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+let AppEntryPoint = App;
+// make it true to render storybook
+// let isStoryBook = false;
+let isStoryBook = true;
+
+if (isStoryBook) {
+  AppEntryPoint = Storybook;
+}
+
+AppRegistry.registerComponent(appName, () => AppEntryPoint);
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -6,6 +6,46 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+// const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// const path = require('path');
+// const { generate } = require('@storybook/react-native/scripts/generate');
+
+// generate({
+//   configPath: path.resolve(__dirname, './.storybook'),
+// });
+
+// module.exports = {
+//   /* existing config */
+//   transformer: {
+//     unstable_allowRequireContext: true,
+//   },
+//   resolver: {
+//     sourceExts: [...defaultConfig.resolver.sourceExts, 'mjs'],
+//   },
+// };
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+const {generate} = require('@storybook/react-native/scripts/generate');
+
+// Generate Storybook configuration
+generate({
+  configPath: path.resolve(__dirname, './.storybook'),
+});
+
+// Get default Metro config
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Merge with additional configurations for Storybook
+const config = {
+  transformer: {
+    unstable_allowRequireContext: true,
+  },
+  resolver: {
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'mjs'],
+  },
+};
+
+// Merge default config with Storybook config
+module.exports = mergeConfig(defaultConfig, config);
